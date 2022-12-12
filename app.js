ff30e94b454840ac97e0fc96f5299e51a8bd970f
Kalle <kalle.paas@voco.ee>
2022-12-12 09:22:57 +0200

    Delete single task from task list

--- app.js
+++ app.js
@@ -1,25 +1,50 @@
 const form = document.querySelector('form')
 form.addEventListener('submit', addTask)
+const tasksList = document.querySelector('.collection')
+tasksList.addEventListener('click', deleteTask)
+
+function deleteTask(event){
+    if(event.target.textContent == 'X'){
+        if(confirm("Do you want to delete this task?")){
+            event.target.parentElement.remove();
+        }
+    }
+}
 
 function addTask(event) {
     const taskText = document.querySelector('#task').value
 
+    // create <li> element
     let li = document.createElement('li')
+    // define <li> CSS class
     li.className = 'collection-item'
+    // create text value for <li>
     let liText = document.createTextNode(taskText)
+    // add text value to <li>
     li.appendChild(liText)
+    // create link element
     let a = document.createElement('a')
-    a.className = 'waves-effect waves-light btn-flat btn-small black-text secondary-content'
+    // add css style
+    a.className = 'waves-effect waves-light black-text secondary-content'
+    // add X text to link
     let aText = document.createTextNode('X')
+    // ???
     a.appendChild(aText)
+    // set href attribute
     a.setAttribute('href', "#")
-
+    // add link to <li>
     li.appendChild(a)
 
-    const ul = document.querySelector('ul')
-    ul.appendChild(li)
 
+
+
+    // find <ul> DOM component
+    const ul = document.querySelector('ul')
+    // add <li> to <ul>
+    ul.appendChild(li)
+    // clear input value
     document.querySelector('#task').value = ''
+    // form submit event control?
     event.preventDefault()
 
 }
