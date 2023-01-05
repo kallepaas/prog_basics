f4a86c4d46146376feed68b5448e9b862ab62eda
Kalle <kalle.paas@voco.ee>
2023-01-05 15:05:51 +0200

    Delete all tasks from localStorage

--- app.js
+++ app.js
@@ -6,10 +6,68 @@ form.addEventListener('submit', addTask)
 tasksList.addEventListener('click', deleteTask)
 delTasksBtn.addEventListener('click', deleteTasks)
 
+document.addEventListener('DOMContentLoaded', getTasks)
+
+function getTasks(event){
+    // ADD TASK TO LOCALSTORAGE
+    let tasks // arrary for user inputs
+    if(localStorage.getItem('tasks') === null){
+        tasks = []
+    } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'))
+    }
+    // loop array for each element value
+
+    tasks.forEach(function (task){
+    // create <li> element
+    let li = document.createElement('li')
+    // define <li> CSS class
+    li.className = 'collection-item'
+    // create text value for <li>
+    let liText = document.createTextNode(taskText)
+    // add text value to <li>
+    li.appendChild(liText)
+    // create link element
+    let a = document.createElement('a')
+    // add css style
+    a.className = 'waves-effect waves-light black-text secondary-content'
+    // add X text to link
+    let aText = document.createTextNode('X')
+    // ???
+    a.appendChild(aText)
+    // set href attribute
+    a.setAttribute('href', "#")
+    // add link to <li>
+    li.appendChild(a)
+
+        ul.appendChild(li)
+
+    })
+
+}
+
 function deleteTask(event){
-    if(event.target.textContent == 'X'){
-        if(confirm("Do you want to delete this task?")){
-            event.target.parentElement.remove();
+    if(event.target.textContent === 'X'){
+        if(confirm('Are you sure to delete this task?')){
+            event.target.parentElement.remove()
+            // get task value from dom li element textContent
+            let liText = event.target.parentElement.textContent
+            let liTextCorrect = liText.slice(0, liText.length-1)
+            // get data from LS
+            let tasks // array for user inputs
+            if(localStorage.getItem('tasks') === null){
+                tasks = []
+            } else {
+                tasks = JSON.parse(localStorage.getItem('tasks'))
+            }
+            // loop all tasks elements
+            tasks.forEach(function(task, index){
+                // get element with the same value as clicked
+                if(task === liTextCorrect){
+                    tasks.splice(index, 1) // delete this element from data array
+                }
+            })
+            localStorage.setItem('tasks', JSON.stringify(tasks)) // save updated data lo LS
         }
     }
 }
@@ -25,8 +83,10 @@ function deleteTasks(){
 }
 
 function addTask(event) {
+    // USER INPUT
     const taskText = document.querySelector('#task').value
 
+    // ADD DOM ELEMENT - begin
     // create <li> element
     let li = document.createElement('li')
     // define <li> CSS class
@@ -49,12 +109,23 @@ function addTask(event) {
     li.appendChild(a)
 
 
-
-
     // find <ul> DOM component
     const ul = document.querySelector('ul')
     // add <li> to <ul>
     ul.appendChild(li)
+
+    // ADD TASK TO LOCALSTORAGE
+    let tasks // arrary for user inputs
+    if(localStorage.getItem('tasks') === null){
+        tasks = []
+} else {
+        tasks = JSON.parse(localStorage.getItem('tasks'))
+    }
+    console.log(tasks)
+    tasks.push(taskText)
+    console.log(tasks)
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+
     // clear input value
     document.querySelector('#task').value = ''
     // form submit event control?
